705a70dabdfe35128853e77ec18835a19edc8bb1
TMulekezi <tlm233@student.bham.ac.uk>
2023-11-26 23:35:01 +0000

    Adding book search functionality

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './layouts/NavBarAndFooter/Navbar';
 import { HomePage } from './layouts/HomePage/Homepage';
 import { Footer } from './layouts/NavBarAndFooter/Footer';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
